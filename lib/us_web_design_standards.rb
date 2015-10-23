3cd1ace481fa627be9b01e492d3f67614b61174b
Mike Bland <michael.bland@gsa.gov>
2015-10-23 11:34:45 -0400

    Add Rails support

--- lib/us_web_design_standards.rb
+++ lib/us_web_design_standards.rb
@@ -2,3 +2,10 @@ require_relative 'us_web_design_standards/assets'
 require_relative 'us_web_design_standards/generator'
 require_relative 'us_web_design_standards/sass'
 require_relative 'us_web_design_standards/version'
+
+module USWebDesignStandards
+  module Rails
+    class Engine < ::Rails::Engine
+    end
+  end
+end
--- lib/us_web_design_standards/generator.rb
+++ lib/us_web_design_standards/generator.rb
@@ -1,9 +1,12 @@
-require 'jekyll'
+begin
+  require 'jekyll'
 
-module USWebDesignStandards
-  class Generator < ::Jekyll::Generator
-    def generate(site)
-      Assets.copy_to_site site
+  module USWebDesignStandards
+    class Generator < ::Jekyll::Generator
+      def generate(site)
+        Assets.copy_to_site site
+      end
     end
   end
+rescue LoadError
 end
