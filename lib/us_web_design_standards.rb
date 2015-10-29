bad5db81da67a4ca717b17eca4e706b390343dfe
Mike Bland <michael.bland@gsa.gov>
2015-10-29 17:12:29 -0400

    Undo scss relative imports; add rails-fonts.css

--- lib/us_web_design_standards.rb
+++ lib/us_web_design_standards.rb
@@ -3,9 +3,12 @@ require_relative 'us_web_design_standards/generator'
 require_relative 'us_web_design_standards/sass'
 require_relative 'us_web_design_standards/version'
 
-module USWebDesignStandards
-  module Rails
-    class Engine < ::Rails::Engine
+begin
+  module USWebDesignStandards
+    module Rails
+      class Engine < ::Rails::Engine
+      end
     end
   end
+rescue NameError
 end
