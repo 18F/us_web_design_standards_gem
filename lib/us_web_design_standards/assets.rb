fda49b53c2d682cf17c2c809295a10ec94160f7f
Jacob Harris <jacob.harris@gsa.gov>
2016-01-13 09:50:28 -0500

    Define a method for returning the images_path
    This is needed for the Rails asset pipeline to know where images are
    located within this gem

--- lib/us_web_design_standards/assets.rb
+++ lib/us_web_design_standards/assets.rb
@@ -1,10 +1,17 @@
 begin
   require 'jekyll/static_file'
+rescue LoadError
+end
 
+begin
   module USWebDesignStandards
     class Assets
       SOURCE = File.realpath File.join(__FILE__, '..', '..', '..')
       BEGIN_PATH = SOURCE.size + File::SEPARATOR.size
+
+      def self.images_path
+        File.expand_path("../../assets/img", __FILE__)
+      end
 
       def self.copy_to_site(site)
         Dir.glob File.join(SOURCE, 'assets', '[^_]*', '**', '*') do |asset|
