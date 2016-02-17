0b09a017a2504c8e54fdff10383037908d8979d9
Roger Steve Ruiz <roger.ruiz@gsa.gov>
2016-02-17 16:57:06 -0500

    Update Sass directory path

--- lib/us_web_design_standards/sass.rb
+++ lib/us_web_design_standards/sass.rb
@@ -2,7 +2,7 @@ require 'sass'
 
 module USWebDesignStandards
   BASEDIR = File.dirname File.dirname(File.dirname(__FILE__))
-  SASS_DIR = File.join BASEDIR, 'assets', '_scss'
+  SASS_DIR = File.join BASEDIR, 'assets', 'sass'
 end
 
 Sass.load_paths << USWebDesignStandards::SASS_DIR
